fd549b768f8710d46d0c1d30faa42947d9d02d8f
Minh-lab <minhn6673@gmail.com>
2026-05-21 13:33:50 +0700

    implement auth feature

---
+++ lib/src/features/auth/domain/entities/user.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+enum AuthProvider { google, facebook, email, anonymous }
+
+class UserEntity extends Equatable {
+  final String uid; // Firebase UID, là khóa định danh duy nhất
+  final String? email; // null nếu là tài khoản Anonymous
+  final String?
+  displayName; // Tên hiển thị (lấy từ Google/Facebook hoặc tự đặt)
+  final String? photoUrl; // Ảnh đại diện (lấy từ Google/Facebook)
+  final bool isAnonymous; // true nếu đang ở chế độ khách
+  final bool isEmailVerified; // true nếu đã xác minh email
+  final AuthProvider provider; // Phương thức đăng nhập hiện tại
+  final DateTime? createdAt;
+
+  const UserEntity({
+    required this.uid,
+    this.email,
+    this.displayName,
+    this.photoUrl,
+    required this.isAnonymous,
+    required this.isEmailVerified,
+    required this.provider,
+    this.createdAt,
+  });
+
+  bool get isAuthenticated => !isAnonymous;
+
+  @override
+  List<Object?> get props => [
+    uid,
+    email,
+    displayName,
+    photoUrl,
+    isAnonymous,
+    isEmailVerified,
+    provider,
+    createdAt,
+  ];
+}
